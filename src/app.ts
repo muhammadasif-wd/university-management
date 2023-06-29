c75c2481630e30e7accede77ea6bb64b707fc039
muhammadasif-wd <muhammadasif.wd@gmail.com>
2023-06-30 00:07:25 +0600

    [DONE] husky configure, eslint, typescript

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,15 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 const port = 3000
+console.log('port', port)
 // cors call
-app.use(cors())
+    app.use(cors())
 // body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 app.get('/', (req: Request, res: Response) => {
-    res.send('University Management System Run Successfully!')
+  res.send('University Management System Run Successfully!')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
+import mongoose from 'mongoose'
+import app from './app'
 import config from './config/index'
 
-
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string)
-        console.log('Connection', `WoW Successfully your database🚪connect...♻️`)
-        //  
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log('Error:', "Failed❌ to connect your database🚪", err)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Connection', `WoW Successfully your database🚪connect...♻️`)
+    //
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Error:', 'Failed❌ to connect your database🚪', err)
+  }
 }
 
-main;
+main
